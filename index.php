0581fc6cdfbefb543ca8ed555681a7ea9c38a505
chamarasab <chamara.sab@outlook.com>
2023-02-03 13:10:07 -0800

    Update Completed ✔

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
+$connection = mysqli_connect("localhost", "root", "", "orbit");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $connection = mysqli_connect("localhost", "root", "", "orbit");
-
     if (!$connection) {
         $_SESSION["message"] = "connection_failed";
     } else {
@@ -28,5 +27,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     #GET
     $_SESSION["message"] = "welcome";
+
+    /**
+     * find user
+     */
+    $user_id = null;
+    $user_name = "";
+    $user_contact = "";
+
+    if(isset($_GET["id"])){
+        $user_id = $_GET["id"];
+        $select_query = "SELECT * from users WHERE id = '".$user_id."'";
+        $result = mysqli_query($connection, $select_query);
+        if ($result) {
+            $_SESSION["message"] = "user_found_success";
+
+            while($user = mysqli_fetch_assoc($result)){
+                $user_id = $user['id'];
+                $user_name = $user['name'];
+                $user_contact = $user['phone'];
+            }
+        } else {
+            $_SESSION["message"] = "user_found_failed";
+        }
+    }
+
 }
 include_once 'insert.html';
