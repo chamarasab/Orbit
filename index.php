968e074dd7096c289f2c3f582102ed1b4cc871f6
chamarasab <chamara.sab@outlook.com>
2023-02-03 09:30:40 -0800

    Insert-UI ✔

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $connection = mysqli_connect("localhost", "root", "", "orbit");
+
+    if (!$connection) {
+        $_SESSION["message"] = "connection_failed";
+    } else {
+        if (!empty($_POST["name"]) and !empty($_POST["phone"])) {
+            $name = $_POST["name"];
+            $phone = $_POST["phone"];
+
+            $insert_query = "INSERT INTO users (name, phone) VALUES ('" . $name . "','" . $phone . "'); ";
+            $result = mysqli_query($connection, $insert_query);
+
+            if ($result) {
+                $_SESSION["message"] = "insert_success";
+                //sleep(2);
+                //header('location:dashboard.php');
+            } else {
+                $_SESSION["message"] = "insert_error";
+            }
+        } else {
+            $_SESSION["message"] = "empty_inputs";
+        }
+    }
+} else {
+    #GET
+    $_SESSION["message"] = "welcome";
+}
+include_once 'insert.html';
